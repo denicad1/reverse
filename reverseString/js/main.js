9ba91dd16e09c883e0faa77edcf9ae1e3553cd97
Anthony Denicolo <denicoloa@live.bucks.edu>
2018-06-28 11:53:48 -0400

    added comments

--- reverseString/js/main.js
+++ reverseString/js/main.js
@@ -1,1 +1,12 @@
-﻿// Your code here!
+﻿//function to reverse a string
+function reverse(string) {
+    //split turns the string into an array which each char is a position on the array
+    var split = string.split("");
+    //rev reverses the array 
+    var rev = split.reverse();
+    //comb turns the array back into a string
+    var comb = rev.join("");
+    //we then log it onto the console
+    console.log(comb);
+};
+reverse("hello world");
